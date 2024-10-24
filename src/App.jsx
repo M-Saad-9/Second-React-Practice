bb5781b1dd4ff8d639695df455dee503a8eb80ad
Muhammad saad <saadrahimit789@gmail.com>
2024-10-24 11:05:25 -0700

    Second react practice

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='intro'>
+      <h1>Welcome to my website!</h1>
+    </div>
+    <p className='summary'>
+    You can find my thoughts here.
+    <br /><br />
+    <b>And <i>picture</i></b> of scientists!
+    </p>
+   </>
+  )
+}
+
+export default App
